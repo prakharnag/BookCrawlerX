f32102dd20b0ae2cc03932da069faf273effcbc8
prakharnag <prakharnag@yahoo.in>
2024-04-17 16:35:29 -0500

    first commit to web scrapping

---
+++ indeedscraper/indeedscraper/spiders/indeedspider.py
@@ -0,0 +1,24 @@
+import scrapy
+import requests
+
+class IndeedspiderSpider(scrapy.Spider):
+    name = "indeedspider"
+    allowed_domains = ["www.indeed.com"]
+    start_urls = ["https://www.indeed.com/jobs"]
+    headers = {
+    'authority': 'scrapeme.live',
+    'dnt': '1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'sec-fetch-site': 'none',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-user': '?1',
+    'sec-fetch-dest': 'document',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+}
+    response = requests.get(start_urls, headers=headers)
+ 
+    }
+    def parse(self, response):
+        pass
